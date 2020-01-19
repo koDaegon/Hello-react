096172e1485277a548a048c96363cd34ee1ebfe9
koDaegon <daegon7897@gmail.com>
2020-01-19 18:15:15 +0900

    Update lazy loading method

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch , Redirect} from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from  '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => import('./NewPost/NewPost'));
+// const LazyNewPost = React.lazy(()=> import('./NewPost/NewPost'));
 
 class Blog extends Component {
     state ={
@@ -36,7 +41,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={()=> (<h1> Page Not Found</h1>)}/> 
                     {/* <Redirect from="/" to="/posts"/> */}
